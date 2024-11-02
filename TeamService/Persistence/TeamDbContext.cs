f1b33bf9f2d887a988d26cddebf495a162d6045e
alikazmi181293 <syed.ali.hussnain.kazmi@gmail.com>
2024-11-02 21:27:33 +0200

    Added DbContext in the project and registered the dv context in the program.cs file. I have also added the necessary nuget packages for entity framework.

---
+++ TeamService/Persistence/TeamDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TeamService.Models.Entities;
+
+namespace TeamService.Persistence
+{
+    public class TeamDbContext : DbContext
+    {
+        DbSet<Team> Teams { get; set; }
+        DbSet<TeamParticipant> TeamParticipants { get; set; }
+
+        public TeamDbContext(DbContextOptions<TeamDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
