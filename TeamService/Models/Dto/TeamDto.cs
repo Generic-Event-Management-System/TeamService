4125f11e81ea6734a87738c17928d2037cdb9458
alikazmi181293 <syed.ali.hussnain.kazmi@gmail.com>
2024-11-02 23:32:48 +0200

    Create team dto class to receive request entities.

---
+++ TeamService/Models/Dto/TeamDto.cs
@@ -0,0 +1,11 @@
+﻿using TeamService.Models.Entities;
+
+namespace TeamService.Models.Dto
+{
+    public class TeamDto
+    {
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+        public required ICollection<int> Participants { get; set; }
+    }
+}
