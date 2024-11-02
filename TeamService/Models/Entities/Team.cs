0dbeba5257bef8476004ef7b4ab0321bd7db5e59
alikazmi181293 <syed.ali.hussnain.kazmi@gmail.com>
2024-11-02 21:25:49 +0200

    Added Team and TeamParticipant entities in the project.

---
+++ TeamService/Models/Entities/Team.cs
@@ -0,0 +1,10 @@
+﻿namespace TeamService.Models.Entities
+{
+    public class Team
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+        public required ICollection<TeamParticipant> Participants { get; set; }
+    }
+}
---
+++ TeamService/Models/Entities/TeamParticipant.cs
@@ -0,0 +1,9 @@
+﻿namespace TeamService.Models.Entities
+{
+    public class TeamParticipant
+    {
+        public int Id { get; set; }
+        public required int ParticipantId { get; set; }
+        public required Team Team { get; set; }
+    }
+}
